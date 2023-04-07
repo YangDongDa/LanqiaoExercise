064471faa3657b2de99c2a513753b42fc4c7c7c9
YangDongDa <2722978054@qq.com>
2023-04-07 10:06:55 +0800

    03fix

--- 2022lanqiao/03.cpp
+++ 2022lanqiao/03.cpp
@@ -2,20 +2,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 long long a[200001];
+long long SUM;
 int main (int argc, char *argv[])
 {
     int n;
     cin >> n;
     for(int i = 1; i <= n; i++){
         cin >> a[i];
+        SUM = SUM + a[i];
     }
     long long sum = 0;
+    long long temp = 0;
     for(int i = 1; i <= n; i++){
-        int temp = 0;
-        for(int j = i + 1; j <= n; j++){
-            temp = a[j] + temp;
-        }
-        sum = sum + a[i] * temp;
+        temp = temp + a[i];
+        sum = sum + a[i] * (SUM - temp);
+       
     }
     cout << sum <<endl;
     return 0;
